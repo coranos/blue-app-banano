6ca2e79acda03a893938d872b46959aee2cbc3d2
coranos <coranos@gmail.com>
2018-05-02 13:34:11 -0500

    fixing icons

--- src/banano.c
+++ src/banano.c
@@ -7,14 +7,8 @@
 /** */
 static const char NO_KEY0[] = "NO KEY REQUESTED";
 
-/** MAX_TX_TEXT_WIDTH in blanks, used for clearing a line of text */
-static const char TXT_BLANK[] = "                 ";
-
-/** Label when displaying a State transaction */
-static const char TX_NM[] = "State Tx";
-
-/** array of capital letter hex values */
-//static const char HEX_CAP[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', };
+/** */
+static const char KEY_PREFIX_BAN[] = "BAN_";
 
 /** current public key text. */
 static char current_public_key_text[MAX_TX_ICON_WIDTH*MAX_TX_ICON_LINES];
--- src/glyphs.h
+++ src/glyphs.h
@@ -1,11 +1,11 @@
 #ifndef GLYPH_base32_1_BPP
-#define GLYPH_base32_1_WIDTH 6
-#define GLYPH_base32_1_HEIGHT 10
+#define GLYPH_base32_1_WIDTH 5
+#define GLYPH_base32_1_HEIGHT 7
 #define GLYPH_base32_1_BPP 1
 extern
 unsigned int const C_base32_1_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_1_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -14,13 +14,13 @@ const bagl_icon_details_t C_base32_1;
 #endif // GLYPH_base32_1_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_3_BPP
-#define GLYPH_base32_3_WIDTH 6
-#define GLYPH_base32_3_HEIGHT 10
+#define GLYPH_base32_3_WIDTH 5
+#define GLYPH_base32_3_HEIGHT 7
 #define GLYPH_base32_3_BPP 1
 extern
 unsigned int const C_base32_3_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_3_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -29,13 +29,13 @@ const bagl_icon_details_t C_base32_3;
 #endif // GLYPH_base32_3_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_4_BPP
-#define GLYPH_base32_4_WIDTH 6
-#define GLYPH_base32_4_HEIGHT 10
+#define GLYPH_base32_4_WIDTH 5
+#define GLYPH_base32_4_HEIGHT 7
 #define GLYPH_base32_4_BPP 1
 extern
 unsigned int const C_base32_4_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_4_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -44,13 +44,13 @@ const bagl_icon_details_t C_base32_4;
 #endif // GLYPH_base32_4_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_5_BPP
-#define GLYPH_base32_5_WIDTH 6
-#define GLYPH_base32_5_HEIGHT 10
+#define GLYPH_base32_5_WIDTH 5
+#define GLYPH_base32_5_HEIGHT 7
 #define GLYPH_base32_5_BPP 1
 extern
 unsigned int const C_base32_5_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_5_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -59,13 +59,13 @@ const bagl_icon_details_t C_base32_5;
 #endif // GLYPH_base32_5_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_6_BPP
-#define GLYPH_base32_6_WIDTH 6
-#define GLYPH_base32_6_HEIGHT 10
+#define GLYPH_base32_6_WIDTH 5
+#define GLYPH_base32_6_HEIGHT 7
 #define GLYPH_base32_6_BPP 1
 extern
 unsigned int const C_base32_6_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_6_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -74,13 +74,13 @@ const bagl_icon_details_t C_base32_6;
 #endif // GLYPH_base32_6_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_7_BPP
-#define GLYPH_base32_7_WIDTH 6
-#define GLYPH_base32_7_HEIGHT 10
+#define GLYPH_base32_7_WIDTH 5
+#define GLYPH_base32_7_HEIGHT 7
 #define GLYPH_base32_7_BPP 1
 extern
 unsigned int const C_base32_7_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_7_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -89,13 +89,13 @@ const bagl_icon_details_t C_base32_7;
 #endif // GLYPH_base32_7_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_8_BPP
-#define GLYPH_base32_8_WIDTH 6
-#define GLYPH_base32_8_HEIGHT 10
+#define GLYPH_base32_8_WIDTH 5
+#define GLYPH_base32_8_HEIGHT 7
 #define GLYPH_base32_8_BPP 1
 extern
 unsigned int const C_base32_8_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_8_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -104,13 +104,13 @@ const bagl_icon_details_t C_base32_8;
 #endif // GLYPH_base32_8_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_9_BPP
-#define GLYPH_base32_9_WIDTH 6
-#define GLYPH_base32_9_HEIGHT 10
+#define GLYPH_base32_9_WIDTH 5
+#define GLYPH_base32_9_HEIGHT 7
 #define GLYPH_base32_9_BPP 1
 extern
 unsigned int const C_base32_9_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_9_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -119,13 +119,13 @@ const bagl_icon_details_t C_base32_9;
 #endif // GLYPH_base32_9_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_A_BPP
-#define GLYPH_base32_A_WIDTH 6
-#define GLYPH_base32_A_HEIGHT 10
+#define GLYPH_base32_A_WIDTH 5
+#define GLYPH_base32_A_HEIGHT 7
 #define GLYPH_base32_A_BPP 1
 extern
 unsigned int const C_base32_A_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_A_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -134,13 +134,13 @@ const bagl_icon_details_t C_base32_A;
 #endif // GLYPH_base32_A_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_B_BPP
-#define GLYPH_base32_B_WIDTH 6
-#define GLYPH_base32_B_HEIGHT 10
+#define GLYPH_base32_B_WIDTH 5
+#define GLYPH_base32_B_HEIGHT 7
 #define GLYPH_base32_B_BPP 1
 extern
 unsigned int const C_base32_B_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_B_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -149,13 +149,13 @@ const bagl_icon_details_t C_base32_B;
 #endif // GLYPH_base32_B_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_C_BPP
-#define GLYPH_base32_C_WIDTH 6
-#define GLYPH_base32_C_HEIGHT 10
+#define GLYPH_base32_C_WIDTH 5
+#define GLYPH_base32_C_HEIGHT 7
 #define GLYPH_base32_C_BPP 1
 extern
 unsigned int const C_base32_C_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_C_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -164,13 +164,13 @@ const bagl_icon_details_t C_base32_C;
 #endif // GLYPH_base32_C_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_D_BPP
-#define GLYPH_base32_D_WIDTH 6
-#define GLYPH_base32_D_HEIGHT 10
+#define GLYPH_base32_D_WIDTH 5
+#define GLYPH_base32_D_HEIGHT 7
 #define GLYPH_base32_D_BPP 1
 extern
 unsigned int const C_base32_D_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_D_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -179,13 +179,13 @@ const bagl_icon_details_t C_base32_D;
 #endif // GLYPH_base32_D_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_E_BPP
-#define GLYPH_base32_E_WIDTH 6
-#define GLYPH_base32_E_HEIGHT 10
+#define GLYPH_base32_E_WIDTH 5
+#define GLYPH_base32_E_HEIGHT 7
 #define GLYPH_base32_E_BPP 1
 extern
 unsigned int const C_base32_E_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_E_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -194,13 +194,13 @@ const bagl_icon_details_t C_base32_E;
 #endif // GLYPH_base32_E_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_F_BPP
-#define GLYPH_base32_F_WIDTH 6
-#define GLYPH_base32_F_HEIGHT 10
+#define GLYPH_base32_F_WIDTH 5
+#define GLYPH_base32_F_HEIGHT 7
 #define GLYPH_base32_F_BPP 1
 extern
 unsigned int const C_base32_F_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_F_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -209,13 +209,13 @@ const bagl_icon_details_t C_base32_F;
 #endif // GLYPH_base32_F_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_G_BPP
-#define GLYPH_base32_G_WIDTH 6
-#define GLYPH_base32_G_HEIGHT 10
+#define GLYPH_base32_G_WIDTH 5
+#define GLYPH_base32_G_HEIGHT 7
 #define GLYPH_base32_G_BPP 1
 extern
 unsigned int const C_base32_G_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_G_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -224,13 +224,13 @@ const bagl_icon_details_t C_base32_G;
 #endif // GLYPH_base32_G_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_H_BPP
-#define GLYPH_base32_H_WIDTH 6
-#define GLYPH_base32_H_HEIGHT 10
+#define GLYPH_base32_H_WIDTH 5
+#define GLYPH_base32_H_HEIGHT 7
 #define GLYPH_base32_H_BPP 1
 extern
 unsigned int const C_base32_H_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_H_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -239,13 +239,13 @@ const bagl_icon_details_t C_base32_H;
 #endif // GLYPH_base32_H_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_I_BPP
-#define GLYPH_base32_I_WIDTH 6
-#define GLYPH_base32_I_HEIGHT 10
+#define GLYPH_base32_I_WIDTH 5
+#define GLYPH_base32_I_HEIGHT 7
 #define GLYPH_base32_I_BPP 1
 extern
 unsigned int const C_base32_I_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_I_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -254,13 +254,13 @@ const bagl_icon_details_t C_base32_I;
 #endif // GLYPH_base32_I_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_J_BPP
-#define GLYPH_base32_J_WIDTH 6
-#define GLYPH_base32_J_HEIGHT 10
+#define GLYPH_base32_J_WIDTH 5
+#define GLYPH_base32_J_HEIGHT 7
 #define GLYPH_base32_J_BPP 1
 extern
 unsigned int const C_base32_J_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_J_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -269,13 +269,13 @@ const bagl_icon_details_t C_base32_J;
 #endif // GLYPH_base32_J_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_K_BPP
-#define GLYPH_base32_K_WIDTH 6
-#define GLYPH_base32_K_HEIGHT 10
+#define GLYPH_base32_K_WIDTH 5
+#define GLYPH_base32_K_HEIGHT 7
 #define GLYPH_base32_K_BPP 1
 extern
 unsigned int const C_base32_K_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_K_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -284,13 +284,13 @@ const bagl_icon_details_t C_base32_K;
 #endif // GLYPH_base32_K_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_M_BPP
-#define GLYPH_base32_M_WIDTH 6
-#define GLYPH_base32_M_HEIGHT 10
+#define GLYPH_base32_M_WIDTH 5
+#define GLYPH_base32_M_HEIGHT 7
 #define GLYPH_base32_M_BPP 1
 extern
 unsigned int const C_base32_M_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_M_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -299,13 +299,13 @@ const bagl_icon_details_t C_base32_M;
 #endif // GLYPH_base32_M_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_N_BPP
-#define GLYPH_base32_N_WIDTH 6
-#define GLYPH_base32_N_HEIGHT 10
+#define GLYPH_base32_N_WIDTH 5
+#define GLYPH_base32_N_HEIGHT 7
 #define GLYPH_base32_N_BPP 1
 extern
 unsigned int const C_base32_N_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_N_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -314,13 +314,13 @@ const bagl_icon_details_t C_base32_N;
 #endif // GLYPH_base32_N_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_O_BPP
-#define GLYPH_base32_O_WIDTH 6
-#define GLYPH_base32_O_HEIGHT 10
+#define GLYPH_base32_O_WIDTH 5
+#define GLYPH_base32_O_HEIGHT 7
 #define GLYPH_base32_O_BPP 1
 extern
 unsigned int const C_base32_O_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_O_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -329,13 +329,13 @@ const bagl_icon_details_t C_base32_O;
 #endif // GLYPH_base32_O_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_P_BPP
-#define GLYPH_base32_P_WIDTH 6
-#define GLYPH_base32_P_HEIGHT 10
+#define GLYPH_base32_P_WIDTH 5
+#define GLYPH_base32_P_HEIGHT 7
 #define GLYPH_base32_P_BPP 1
 extern
 unsigned int const C_base32_P_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_P_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -344,13 +344,13 @@ const bagl_icon_details_t C_base32_P;
 #endif // GLYPH_base32_P_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_Q_BPP
-#define GLYPH_base32_Q_WIDTH 6
-#define GLYPH_base32_Q_HEIGHT 10
+#define GLYPH_base32_Q_WIDTH 5
+#define GLYPH_base32_Q_HEIGHT 7
 #define GLYPH_base32_Q_BPP 1
 extern
 unsigned int const C_base32_Q_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_Q_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -359,13 +359,13 @@ const bagl_icon_details_t C_base32_Q;
 #endif // GLYPH_base32_Q_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_R_BPP
-#define GLYPH_base32_R_WIDTH 6
-#define GLYPH_base32_R_HEIGHT 10
+#define GLYPH_base32_R_WIDTH 5
+#define GLYPH_base32_R_HEIGHT 7
 #define GLYPH_base32_R_BPP 1
 extern
 unsigned int const C_base32_R_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_R_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -374,13 +374,13 @@ const bagl_icon_details_t C_base32_R;
 #endif // GLYPH_base32_R_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_S_BPP
-#define GLYPH_base32_S_WIDTH 6
-#define GLYPH_base32_S_HEIGHT 10
+#define GLYPH_base32_S_WIDTH 5
+#define GLYPH_base32_S_HEIGHT 7
 #define GLYPH_base32_S_BPP 1
 extern
 unsigned int const C_base32_S_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_S_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -389,13 +389,13 @@ const bagl_icon_details_t C_base32_S;
 #endif // GLYPH_base32_S_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_T_BPP
-#define GLYPH_base32_T_WIDTH 6
-#define GLYPH_base32_T_HEIGHT 10
+#define GLYPH_base32_T_WIDTH 5
+#define GLYPH_base32_T_HEIGHT 7
 #define GLYPH_base32_T_BPP 1
 extern
 unsigned int const C_base32_T_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_T_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -404,13 +404,13 @@ const bagl_icon_details_t C_base32_T;
 #endif // GLYPH_base32_T_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_U_BPP
-#define GLYPH_base32_U_WIDTH 6
-#define GLYPH_base32_U_HEIGHT 10
+#define GLYPH_base32_U_WIDTH 5
+#define GLYPH_base32_U_HEIGHT 7
 #define GLYPH_base32_U_BPP 1
 extern
 unsigned int const C_base32_U_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_U_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -419,13 +419,13 @@ const bagl_icon_details_t C_base32_U;
 #endif // GLYPH_base32_U_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_W_BPP
-#define GLYPH_base32_W_WIDTH 6
-#define GLYPH_base32_W_HEIGHT 10
+#define GLYPH_base32_W_WIDTH 5
+#define GLYPH_base32_W_HEIGHT 7
 #define GLYPH_base32_W_BPP 1
 extern
 unsigned int const C_base32_W_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_W_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -434,13 +434,13 @@ const bagl_icon_details_t C_base32_W;
 #endif // GLYPH_base32_W_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_X_BPP
-#define GLYPH_base32_X_WIDTH 6
-#define GLYPH_base32_X_HEIGHT 10
+#define GLYPH_base32_X_WIDTH 5
+#define GLYPH_base32_X_HEIGHT 7
 #define GLYPH_base32_X_BPP 1
 extern
 unsigned int const C_base32_X_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_X_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -449,13 +449,13 @@ const bagl_icon_details_t C_base32_X;
 #endif // GLYPH_base32_X_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_Y_BPP
-#define GLYPH_base32_Y_WIDTH 6
-#define GLYPH_base32_Y_HEIGHT 10
+#define GLYPH_base32_Y_WIDTH 5
+#define GLYPH_base32_Y_HEIGHT 7
 #define GLYPH_base32_Y_BPP 1
 extern
 unsigned int const C_base32_Y_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_Y_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -464,13 +464,13 @@ const bagl_icon_details_t C_base32_Y;
 #endif // GLYPH_base32_Y_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32_Z_BPP
-#define GLYPH_base32_Z_WIDTH 6
-#define GLYPH_base32_Z_HEIGHT 10
+#define GLYPH_base32_Z_WIDTH 5
+#define GLYPH_base32_Z_HEIGHT 7
 #define GLYPH_base32_Z_BPP 1
 extern
 unsigned int const C_base32_Z_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32_Z_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -479,13 +479,13 @@ const bagl_icon_details_t C_base32_Z;
 #endif // GLYPH_base32_Z_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32__blank_BPP
-#define GLYPH_base32__blank_WIDTH 6
-#define GLYPH_base32__blank_HEIGHT 11
+#define GLYPH_base32__blank_WIDTH 5
+#define GLYPH_base32__blank_HEIGHT 8
 #define GLYPH_base32__blank_BPP 0
 extern
 unsigned int const C_base32__blank_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32__blank_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -494,13 +494,13 @@ const bagl_icon_details_t C_base32__blank;
 #endif // GLYPH_base32__blank_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32__error_BPP
-#define GLYPH_base32__error_WIDTH 6
-#define GLYPH_base32__error_HEIGHT 11
-#define GLYPH_base32__error_BPP 0
+#define GLYPH_base32__error_WIDTH 5
+#define GLYPH_base32__error_HEIGHT 8
+#define GLYPH_base32__error_BPP 1
 extern
 unsigned int const C_base32__error_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32__error_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -509,13 +509,13 @@ const bagl_icon_details_t C_base32__error;
 #endif // GLYPH_base32__error_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_base32__underscore_BPP
-#define GLYPH_base32__underscore_WIDTH 6
-#define GLYPH_base32__underscore_HEIGHT 11
-#define GLYPH_base32__underscore_BPP 0
+#define GLYPH_base32__underscore_WIDTH 5
+#define GLYPH_base32__underscore_HEIGHT 8
+#define GLYPH_base32__underscore_BPP 1
 extern
 unsigned int const C_base32__underscore_colors[]
 ;
-extern
+extern	
 unsigned char const C_base32__underscore_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -524,13 +524,13 @@ const bagl_icon_details_t C_base32__underscore;
 #endif // GLYPH_base32__underscore_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_banano_BPP
-#define GLYPH_icon_banano_WIDTH 52
+#define GLYPH_icon_banano_WIDTH 49
 #define GLYPH_icon_banano_HEIGHT 16
 #define GLYPH_icon_banano_BPP 1
 extern
 unsigned int const C_icon_banano_colors[]
 ;
-extern
+extern	
 unsigned char const C_icon_banano_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -539,13 +539,13 @@ const bagl_icon_details_t C_icon_banano;
 #endif // GLYPH_icon_banano_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_default_BPP
-#define GLYPH_icon_default_WIDTH 6
-#define GLYPH_icon_default_HEIGHT 11
-#define GLYPH_icon_default_BPP 0
+#define GLYPH_icon_default_WIDTH 49
+#define GLYPH_icon_default_HEIGHT 16
+#define GLYPH_icon_default_BPP 1
 extern
 unsigned int const C_icon_default_colors[]
 ;
-extern
+extern	
 unsigned char const C_icon_default_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
@@ -554,13 +554,13 @@ const bagl_icon_details_t C_icon_default;
 #endif // GLYPH_icon_default_BPP
 #endif // OS_IO_SEPROXYHAL
 #ifndef GLYPH_icon_nano_BPP
-#define GLYPH_icon_nano_WIDTH 52
+#define GLYPH_icon_nano_WIDTH 49
 #define GLYPH_icon_nano_HEIGHT 16
 #define GLYPH_icon_nano_BPP 1
 extern
 unsigned int const C_icon_nano_colors[]
 ;
-extern
+extern	
 unsigned char const C_icon_nano_bitmap[];
 #ifdef OS_IO_SEPROXYHAL
 #include "os_io_seproxyhal.h"
